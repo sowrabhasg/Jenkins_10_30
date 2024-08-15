164d1382174321d1fbd8ac2cd10ebb8bb09e0a03
sowrabhasg <147090377+sowrabhasg@users.noreply.github.com>
2024-08-15 11:43:47 +0530

    jenkins 1st

---
+++ JenkinsSection/src/test/java/jenkins/ToLearnJenkins.java
@@ -0,0 +1,21 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnJenkins
+{
+@Test
+	public void sample() 
+	{
+	Reporter.log("jenkins");
+	}
+	
+}
+		
+
+
+
+
+
+
