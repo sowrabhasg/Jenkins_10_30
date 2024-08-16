de1d5efd691530fa85d0a61b4269b765e42d8bf1
sowrabhasg <147090377+sowrabhasg@users.noreply.github.com>
2024-08-16 11:52:23 +0530

    committe

---
+++ JenkinsSection/src/test/java/jenkins/jenkins1.java
@@ -0,0 +1,20 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkins1 
+{
+
+public class ToLearnJenkins
+{
+@Test
+	public void sample() 
+	{
+	Reporter.log("jenkins is executing");
+	}
+	
+}
+		
+
+}
